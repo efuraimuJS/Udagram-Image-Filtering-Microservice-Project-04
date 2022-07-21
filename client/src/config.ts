eec1ba22353a4b4afe676915a87a019dda066e51
efuraimujs <efuraimu.js@gmail.com>
2022-07-21 03:08:51 +0300

    config.ts update

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-74ecthwq.us.auth0.com',            // Auth0 domain
+  clientId: '77cvhweDBZ0HpsCo2aE0oKZSA6zte8zW',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
